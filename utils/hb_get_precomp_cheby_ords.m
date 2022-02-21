c5b449a17709171ff5e5d75a5dbd16da5fe04da0
Hamid Behjat <hamid.behjat@gmail.com>
2022-02-21 16:06:02 +0100

    misc updates

--- utils/hb_get_precomp_cheby_ords.m
+++ utils/hb_get_precomp_cheby_ords.m
@@ -1,8 +1,11 @@
-function [cOrds,opts_cOrds] = hb_get_precomp_cheby_ords(type,N)
-switch type
+function [cOrds,opts,kType] = hb_get_precomp_cheby_ords(N,kType)
+if ~exist('kType','var') || isempty(kType)
+    kType = 'MeyerLike'; % type of uniform kernels for estimating energy
+end
+switch kType
     case 'MeyerLike'
         t = 'meyer';
-    case 'SplineType'
+    case 'SplineType' % not supported yet
         t = 'spline';
 end
 d = sprintf('cOrds_%s_%d.mat',t,N);
@@ -10,20 +13,20 @@ d = which(d);
 if ~isempty(d)
     d = load(d);
     cOrds = d.cOrds;
-    opts_cOrds = struct;
-    opts_cOrds.justCheckKernels = false;
+    opts = struct;
+    opts.justCheckKernels = false;
 else
     cOrds = [];
-    opts_cOrds = struct;
-    opts_cOrds.tol.kernel     = 1e-3;
-    opts_cOrds.tol.tightframe = 1e-3;
-    opts_cOrds.justCheckKernels = false; % Note
-    opts_cOrds.minOrds = 50;
-    opts_cOrds.maxOrds = 600;
-    opts_cOrds.parallelize = true;
+    opts = struct;
+    opts.tol.kernel     = 1e-3;
+    opts.tol.tightframe = 1e-3;
+    opts.justCheckKernels = false; % Note
+    opts.minOrds = 50;
+    opts.maxOrds = 800;
+    opts.parallelize = true;
 end
 end
 
 % Note: unif SOSKS should---to a reasonable degree as determined by allowed
-% error tolerance specified here---form a tight (& Parseval) frame for
-% correct estimation of energy spectral density
+% error tolerance specified here, opts_cOrds.tol---form a tight (&
+% Parseval) frame for correct estimation of energy spectral density
